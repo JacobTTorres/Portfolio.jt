09ecd623d36958118494e863a8b55386792aacd7
JacobTTorres <jacobttorres@hotmail.com>
2022-10-30 16:10:24 -0500

    need to add contact and clean up project links

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$('#impact').mouseenter(function () {
+		$('#impactInfo').fadeIn(200);
+	});
+	$('#impactInfo').mouseleave(function () {
+		$('#impactInfo').fadeOut(200);
+	});
+	$('#sun').mouseenter(function () {
+		$('#sunInfo').fadeIn(200);
+	});
+	$('#sunInfo').mouseleave(function () {
+		$('#sunInfo').fadeOut(200);
+	});
+	$('#loop').mouseenter(function () {
+		$('#loopInfo').fadeIn(200);
+	});
+	$('#loopInfo').mouseleave(function () {
+		$('#loopInfo').fadeOut(200);
+	});
+	$('#google').mouseenter(function () {
+		$('#googleInfo').fadeIn(200);
+	});
+	$('#googleInfo').mouseleave(function () {
+		$('#googleInfo').fadeOut(200);
+	});
+});
